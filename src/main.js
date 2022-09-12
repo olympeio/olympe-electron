193383f48283027f1bf90c42902ea18e4972e668
Syméon del Marmol <symeon.delmarmol@olympe.ch>
2022-09-12 17:05:54 +0200

    Cleaun up + license

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 // Import Olympe runtime or DRAW
 import 'olympe';
 import '@olympeio/core';
-import '@olympeio-extensions/commons';
 
 // Import project bricks. We use webpack-import-glob-loader to import all bricks.
-import './bricks/**/*.js';
+// import './bricks/**/*.js';
